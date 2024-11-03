a8d69003fbf2d3661893f78b44a6c214fcce5a22
JTK01 <99972138+JTK01@users.noreply.github.com>
2024-11-03 15:11:19 +0900

    Update win_utils.py

--- utils/win_utils.py
+++ utils/win_utils.py
@@ -28,6 +28,7 @@ def augment_data(X, y, random_state=42):
     else:
         raise ValueError("모든 클래스 데이터의 열 수가 같지 않거나 데이터가 부족합니다.")
 
+# Random Forest 모델
 def rf_train(original_win_rate, imp_data, w_l_data, data_label, t_s, n_es, max_d, mss, msl, min_inc, max_inc, min_win, max_win):
     # 데이터 분할
     X_train, X_test, y_train, y_test = train_test_split(imp_data, w_l_data, test_size=t_s, random_state=42)
@@ -38,27 +39,41 @@ def rf_train(original_win_rate, imp_data, w_l_data, data_label, t_s, n_es, max_d
     X_test_imputed = imputer.transform(X_test)
 
     # 랜덤 포레스트 모델 정의 (기본 파라미터)
-    rf = RandomForestClassifier(n_estimators=n_es, max_depth=max_d, bootstrap = False, min_samples_split = mss, min_samples_leaf = msl, random_state=42)
+    rf = RandomForestClassifier(
+        n_estimators=n_es, 
+        max_depth=max_d, 
+        bootstrap = False, 
+        min_samples_split = mss, 
+        min_samples_leaf = msl, 
+        random_state=42)
     rf.fit(X_train_imputed, y_train)
 
     y_pred = rf.predict(X_test_imputed)
     accuracy = accuracy_score(y_test, y_pred)
     print(f"Random Forest 모델의 정확도: {accuracy * 100:.2f}%")
 
+    decrease_indices = [0, 1, 4]  # 낮춰야 할 인덱스
+
+    random.seed(42)
+
     # 상위 N개의 중요한 특성에 대해 승률 개선 계산
-    for top_n in range(2, 11):
+    for top_n in range(2, 7):
         importances = np.abs(rf.feature_importances_)
         indices = np.argsort(importances)[::-1]
         top_features_indices = indices[:top_n]
 
-        for increase_ratio in np.arange(min_inc, max_inc, 0.001):
+        for increase_ratio in np.arange(min_inc, max_inc, 0.0005):
             X_test_modified = X_test_imputed.copy()
             original_feature_values = X_test_imputed.mean(axis=0)
             modified_feature_values = original_feature_values.copy()
 
             for idx in top_features_indices:
-                X_test_modified[:, idx] *= (1 + increase_ratio)
-                modified_feature_values[idx] = original_feature_values[idx] * (1 + increase_ratio)
+                if idx in [0, 1, 4]:  # 낮춰야 할 인덱스
+                    X_test_modified[:, idx] *= (1 - increase_ratio)
+                    modified_feature_values[idx] = original_feature_values[idx] * (1 - increase_ratio)
+                else:  # 나머지는 값을 높임
+                    X_test_modified[:, idx] *= (1 + increase_ratio)
+                    modified_feature_values[idx] = original_feature_values[idx] * (1 + increase_ratio)
 
             y_pred_modified = rf.predict(X_test_modified)
             modified_win_count = sum(1 for result in y_pred_modified if result == '승')
@@ -74,7 +89,7 @@ def rf_train(original_win_rate, imp_data, w_l_data, data_label, t_s, n_es, max_d
                 return top_n, increase_ratio, improved_features_text, original_win_rate, modified_win_rate, win_rate_improvement
         
     return 0, 0, "", original_win_rate, original_win_rate, 0
-                
+
 
 # 승률 개선 계산 함수
 def calculate_win_improvement(imp_data, w_l_data, data_label, random_state=42):
@@ -83,10 +98,12 @@ def calculate_win_improvement(imp_data, w_l_data, data_label, random_state=42):
     win_count = sum(1 for result in w_l_data if result == '승')
     original_win_rate = win_count / len(w_l_data)
 
-    if original_win_rate > 0.5:
+    if original_win_rate > 0.667:
         return rf_train(original_win_rate = original_win_rate, imp_data = imp_data, w_l_data = w_l_data, data_label = data_label, 
-        t_s = 0.25, n_es = 7, max_d = 5, mss = 0.17, msl = 0.17, min_inc = 0.03, max_inc = 0.7, min_win = 0.01, max_win = 0.4)
-    elif original_win_rate <= 0.5:
+        t_s = 0.33, n_es = 30, max_d = 15, mss = 0.1, msl = 0.12, min_inc = 0.05, max_inc = 0.5, min_win = 0.05, max_win = 0.3)
+    elif original_win_rate > 0.334:
         return rf_train(original_win_rate = original_win_rate, imp_data = imp_data, w_l_data = w_l_data, data_label = data_label, 
-        t_s= 0.2, n_es = 10, max_d = 5, mss = 0.1, msl = 0.1, min_inc = 0.05, max_inc = 0.7, min_win = 0.01, max_win = 0.6)
-    
+        t_s= 0.2, n_es = 10, max_d = 5, mss = 0.1, msl = 0.12, min_inc = 0.05, max_inc = 0.6, min_win = 0.03, max_win = 0.6)
+    else:
+        return rf_train(original_win_rate = original_win_rate, imp_data = imp_data, w_l_data = w_l_data, data_label = data_label, 
+        t_s= 0.2, n_es = 10, max_d = 5, mss = 0.2, msl = 0.2, min_inc = 0.05, max_inc = 0.7, min_win = 0.05, max_win = 0.7)
