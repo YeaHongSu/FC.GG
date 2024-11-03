d4cb14b802d8dcafe52199a67b3d58a50ced6789
JTK01 <99972138+JTK01@users.noreply.github.com>
2024-11-03 19:44:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,24 @@ from utils.utils import me, you, avg_data, top_n_argmax, top_n_argmin, calculate
 from utils.data_processing import data_list, data_list_cl, data_label, determine_play_style
 from utils.win_utils import calculate_win_improvement
 from tier.tier_info import tier
-from concurrent.futures import ThreadPoolExecutor
+import warnings
+import asyncio
+import aiohttp
 
+
+async def fetch_match_data(session, match_id, headers):
+    url = f"https://open.api.nexon.com/fconline/v1/match-detail?matchid={match_id}"
+    async with session.get(url, headers=headers) as response:
+        return await response.json()
+
+async def fetch_all_match_data(matches, headers):
+    async with aiohttp.ClientSession() as session:
+        tasks = [fetch_match_data(session, match_id, headers) for match_id in matches]
+        return await asyncio.gather(*tasks)
+
+def get_match_data(matches, headers):
+    return asyncio.run(fetch_all_match_data(matches, headers))
+   
 # Flask 선언
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -20,6 +36,8 @@ def home():
    if request.method == 'POST':
       character_name = request.form.get('character_name')
       session['character_name'] = character_name
+      match_type = request.form.get('match_type')
+      session['match_type'] = match_type
       return redirect(url_for('result'))
    return render_template('home.html')
 
@@ -35,7 +53,10 @@ def result():
 
         # 닉네임에서 띄어쓰기 제거
         character_name = character_name.replace(" ", "")
-
+       
+        # match 종류 확인
+        match_type = request.args.get('match_type')
+       
         # API key 설정
         headers = {"x-nxopen-api-key" : f"{app.config['API_KEY']}"}
         
@@ -71,10 +92,12 @@ def result():
             {"divisionId": 3000, "divisionName": "https://ssl.nexon.com/s2/game/fo4/obt/rank/large/update_2009/ico_rank16.png"},
             {"divisionId": 3100, "divisionName": "https://ssl.nexon.com/s2/game/fo4/obt/rank/large/update_2009/ico_rank17.png"}
         ]
-        # matchType 50의 division 가져오기
-        match_type_50 = next((item for item in division_info if item["matchType"] == 50), None)
-        if match_type_50:
-            tier_id = match_type_50.get("division", "정보 없음")
+        
+        # matchType의 division 가져오기
+        match_type_info = next((item for item in division_info if item["matchType"] == int(match_type)), None)
+       
+        if match_type_info:
+            tier_id = match_type_info.get("division", "정보 없음")
             # divisionId로 divisionName을 찾아서 level_data에 추가
             division_item = next((item for item in division_mapping if item["divisionId"] == tier_id), None)
             if division_item:
@@ -100,31 +123,18 @@ def result():
 
 
         # 유저 매치 데이터 20개 불러와 저장 (전적 띄우기 용 + 중요 정보 저장)
-        urlString = "https://open.api.nexon.com/fconline/v1/user/match?ouid=" + characterName + "&matchtype=50&limit=20"
+        urlString = "https://open.api.nexon.com/fconline/v1/user/match?ouid=" + characterName + "&matchtype="+match_type+"&limit=20"
         response = requests.get(urlString, headers=headers)
         matches = response.json()
 
         if len(matches) == 0:
-            flash("경기 수가 부족하여 검색이 불가능합니다")
-            return render_template('home.html')
-        
-        # 매치 데이터를 병렬로 가져오기 위한 함수 정의
-        def fetch_match_data(match_id):
-            urlString = f"https://open.api.nexon.com/fconline/v1/match-detail?matchid={match_id}"
-            response = requests.get(urlString, headers=headers)
-            return response.json()
+            return render_template('result.html', level_data=level_data, no_recent_matches=True)
 
-        # ThreadPoolExecutor를 사용해 API 요청 병렬 처리
-        with ThreadPoolExecutor(max_workers=20) as executor:
-            match_data_list = list(executor.map(fetch_match_data, matches))
+        match_data_list = get_match_data(matches, headers)
 
         result_list = []
         imp_data = []
 
-        # for i in matches:
-        #     urlString = "https://open.api.nexon.com/fconline/v1/match-detail?matchid=" + i
-        #     response = requests.get(urlString, headers=headers)
-        #     data = response.json()
         # 각 매치 데이터 처리
         for data in match_data_list:
             date = calculate_time_difference(data['matchDate'])
@@ -136,7 +146,6 @@ def result():
                continue
             w_l = my_data['matchDetail']['matchResult']
 
-            # 비정상 게임 3:0으로 처리
             # 비정상 게임 3:0으로 처리
             my_goal_total = my_data['shoot']['goalTotal'] if my_data['shoot']['goalTotal'] is not None else 0
             your_goal_total = your_data['shoot']['goalTotal'] if your_data['shoot']['goalTotal'] is not None else 0
@@ -156,8 +165,8 @@ def result():
         my_avg = np.nanmean(imp_data, axis=0)
 
         # 전체 유저 중요 지표 평균 불러오기
-        cl_data = np.array(data_list_cl(avg_data()))
-
+        cl_data = np.array(data_list_cl(avg_data(match_type)))
+       
         # 상위/하위 10개 중요 지표 선정
         jp_num = 20  # 먼저 10개의 지표를 가져옴
         threshold = 0.9  # 극단적인 차이를 제외하기 위한 임계값 설정
@@ -188,11 +197,12 @@ def result():
         # 플레이스타일 결정
         play_style = determine_play_style(max_data, min_data)
 
-        return render_template('result.html', my_data=my_data, match_data=result_list, level_data=level_data, 
+        return render_template('result.html', my_data=my_data, match_data=result_list, level_data=level_data, match_type = match_type,
                             max_data=max_data, min_data=min_data, data_label=data_label, jp_num=jp_num,
                             play_style=play_style)
 
     except Exception as e:
+        print(e)
         flash("닉네임이 존재하지 않거나 경기 수가 부족하여 검색이 불가능합니다.")
         return render_template('home.html')
 
@@ -214,6 +224,9 @@ def wr_result():
         # 닉네임에서 띄어쓰기 제거
         character_name = character_name.replace(" ", "")
 
+        # match 종류 확인
+        match_type = request.args.get('match_type')
+       
         headers = {"x-nxopen-api-key": f"{app.config['API_KEY']}"}
         urlString = "https://open.api.nexon.com/fconline/v1/id?nickname=" + character_name
         characterName = requests.get(urlString, headers=headers).json()["ouid"]
@@ -226,7 +239,7 @@ def wr_result():
             "level": lv
         }
 
-        urlString = "https://open.api.nexon.com/fconline/v1/user/match?ouid=" + characterName + "&matchtype=50&limit=20"
+        urlString = "https://open.api.nexon.com/fconline/v1/user/match?ouid=" + characterName + "&matchtype=" + match_type + "&limit=20"
         response = requests.get(urlString, headers=headers)
         matches = response.json()
 
@@ -234,16 +247,8 @@ def wr_result():
             flash("경기 수가 부족하여 검색이 불가능합니다 (최소 5경기가 필요합니다)")
             return redirect(url_for('home'))
            
-        # 매치 데이터를 병렬로 가져오기 위한 함수 정의
-        def fetch_match_data(match_id):
-            urlString = f"https://open.api.nexon.com/fconline/v1/match-detail?matchid={match_id}"
-            response = requests.get(urlString, headers=headers)
-            return response.json()
-
-        # ThreadPoolExecutor를 사용해 API 요청 병렬 처리
-        with ThreadPoolExecutor(max_workers=20) as executor:
-            match_data_list = list(executor.map(fetch_match_data, matches))
-           
+        match_data_list = get_match_data(matches, headers)
+        
         result_list = []
         imp_data = []
         w_l_data = []
@@ -268,7 +273,7 @@ def wr_result():
             w_l_data.append(w_l)
 
         my_avg = np.nanmean(imp_data, axis=0)
-        cl_data = np.array(data_list_cl(avg_data()))
+        cl_data = np.array(data_list_cl(avg_data(match_type)))
 
         jp_num = 20
         threshold = 0.9
@@ -296,6 +301,7 @@ def wr_result():
                                original_win_rate=original_win_rate, modified_win_rate=modified_win_rate, win_rate_improvement=win_rate_improvement)
 
     except Exception as e:
+        print(e)
         flash("닉네임이 존재하지 않거나 경기 수가 부족하여 검색이 불가능합니다.")
         return redirect(url_for('home'))
 
